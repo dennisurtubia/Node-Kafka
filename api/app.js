488c038ac12ab6cd3f7e76ab8f85c6adcdc89ecb
dennisurtubia <dennisurtubia@alunos.utfpr.edu.br>
2019-11-16 12:26:37 -0300

    Arquivo de servidor e app

---
+++ api/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const apiRouter = require('./src/routes');
+
+const app = express();
+
+app.use('/api', apiRouter);
+app.use(cors({ credentials: true }));
+
+module.exports = app;
