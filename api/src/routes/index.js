09ec4c822ea287f70f3ea49a99205fb3175c7143
dennisurtubia <dennisurtubia@alunos.utfpr.edu.br>
2019-11-16 12:24:21 -0300

    Adiciona rotas para para produtos e vendas

---
+++ api/src/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { errors } = require('celebrate');
+const validationMiddleware = require('../middlewares/validations');
+
+const productsRoutes = require('./products');
+const shoppingRoutes = require('./shopping');
+
+const router = express.Router();
+router.use(express.json());
+
+router.use('/products', productsRoutes);
+router.use('/shopping', shoppingRoutes);
+
+router.use('/', validationMiddleware.validation);
+router.use(errors());
+
+module.exports = router;
---
+++ api/src/routes/products.js
@@ -0,0 +1,11 @@
+
+const router = require('express').Router();
+const controller = require('../controllers/product');
+const validations = require('../validations/product');
+
+router.get('/', controller.getAll);
+router.get('/:productId', controller.getById);
+router.post('/', validations.add(), controller.add);
+router.delete('/:productId', controller.delete);
+
+module.exports = router;
