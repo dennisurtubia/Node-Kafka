b38d4af764e80fc9528ffcf1ca8ddcc2c902caea
dennisurtubia <dennisurtubia@alunos.utfpr.edu.br>
2019-11-16 12:53:52 -0300

    Correção da validação para rota de compras

--- api/src/validations/shopping.js
+++ api/src/validations/shopping.js
@@ -5,13 +5,13 @@ module.exports = {
   sale() {
     return celebrate({
       body: Joi.object().keys({
-        produto_id: Joi.number().required(),
-        qtde_comprada: Joi.number().min(1).required(),
-        cartao: Joi.object().required().keys({
-          titular: Joi.string().required(),
-          numero: Joi.string().required(),
-          data_expiracao: Joi.string().required(),
-          bandeira: Joi.string().required(),
+        product_id: Joi.number().required(),
+        amount_purchased: Joi.number().min(1).required(),
+        card: Joi.object().required().keys({
+          cardholder: Joi.string().required(),
+          number: Joi.string().required(),
+          expiration_date: Joi.string().required(),
+          banner: Joi.string().required(),
           cvv: Joi.string().required(),
         }),
       }),
